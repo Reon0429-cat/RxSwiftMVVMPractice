9c70f52de181211510bc900e61ccbfe181cfd2eb
Reon0429-cat <oonishireon110@gmail.com>
2021-09-24 07:42:31 +0900

    合成オペレーター練習

--- RxSwiftMVVMPractice/Sample/SampleViewController.swift
+++ RxSwiftMVVMPractice/Sample/SampleViewController.swift
@@ -14,14 +14,74 @@ enum SampleError: Error {
 }
 
 final class SampleViewController: UIViewController {
+    
+    private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        combineLatest()
+        just()
         
     }
     
+    private func just() {
+        
+    }
+    
+    private func createObservable() {
+        // ライブラリ自体にObservableが用意されている場合はそちらを使うが、
+        // 用意されていない場合は自作する
+        // ↓ subscribeされると100, 200を出力後、完了するObservableを自作
+        let sample1 = Observable<Int>.create { observer in
+            observer.onNext(100)
+            observer.onNext(200)
+            observer.onCompleted()
+            return Disposables.create()
+        }
+        sample1
+            .subscribe({ print($0) })
+            .disposed(by: disposeBag)
+        // next(100)
+        // next(200)
+        // completed
+    }
+    
+    private func zip() {
+        // 複数のオブザーバブルのイベントを一つずつ順番に合成する
+        // 並列処理をする場合に使う
+        // (API通信で複数のレスポンスを待ってから処理を進めたい時など)
+        let int = Observable.of(1, 2, 3, 4)
+        let string = Observable.of("a", "b", "c")
+        _ = Observable.zip(int, string) {
+            "\($0)" + "\($1)"
+        }
+        .subscribe { print("onNext", $0) }
+        .disposed(by: disposeBag)
+        // onNext next(1a)
+        // onNext next(2b)
+        // onNext next(3c)
+        // onNext completed
+    }
+    
+    private func withLatestFrom() {
+        // あるオブザーバブルがイベントを送信した際にもう一方のオブザーバブルの最新イベントを合成
+        // 一つのアクションが起きた時に他の値に対して何かしらの処理をする場合に使う
+        // (ボタンが押された時に、他の要素がどのようになっているか知りたいときなど)
+        let int = PublishSubject<Int>()
+        let string = PublishSubject<String>()
+        int.withLatestFrom(string)
+            .subscribe({ string in
+                print("onNext", string)
+            })
+            .disposed(by: disposeBag)
+        
+        int.onNext(1)
+        string.onNext("a")
+        int.onNext(2)
+        string.onNext("b")
+        string.onNext("c")
+        int.onNext(3)
+    }
     
     private func combineLatest() {
         // 複数の変数を監視する必要がある場合に使う合成オペレーター
@@ -30,7 +90,10 @@ final class SampleViewController: UIViewController {
         let string = PublishSubject<String>()
         let _ = Observable.combineLatest(int, string) {
             "\($0)" + "\($1)"
-        }.subscribe {print("onNext", $0) }
+        }
+            .subscribe { print("onNext", $0) }
+            .disposed(by: disposeBag)
+        
         int.onNext(1)
         string.onNext("a")
         int.onNext(2)
