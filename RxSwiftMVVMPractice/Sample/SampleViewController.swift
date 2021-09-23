b9e8f41ad04ad26d64f044b67d226884595803a6
Reon0429-cat <oonishireon110@gmail.com>
2021-09-24 07:18:57 +0900

    combineLatest練習

--- RxSwiftMVVMPractice/Sample/SampleViewController.swift
+++ RxSwiftMVVMPractice/Sample/SampleViewController.swift
@@ -18,6 +18,28 @@ final class SampleViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        combineLatest()
+        
+    }
+    
+    
+    private func combineLatest() {
+        // 複数の変数を監視する必要がある場合に使う合成オペレーター
+        // (ログイン画面のバリデーションなど)
+        let int = PublishSubject<Int>()
+        let string = PublishSubject<String>()
+        let _ = Observable.combineLatest(int, string) {
+            "\($0)" + "\($1)"
+        }.subscribe {print("onNext", $0) }
+        int.onNext(1)
+        string.onNext("a")
+        int.onNext(2)
+        string.onNext("b")
+        string.onNext("c")
+        int.onNext(3)
+    }
+    
+    private func single() {
         let single = Single<Int>.create { singleObserver in
             singleObserver(.success(100))
             return Disposables.create()
@@ -50,7 +72,6 @@ final class SampleViewController: UIViewController {
                     print("error\(error)")
             }
         }
-        
     }
     
 }
